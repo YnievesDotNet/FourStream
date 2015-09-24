cc7b0ec0e526e4d7a30287456d07a679a150a8a7
Yoinier Hernandez Nieves <ynieves@ynieves.net>
2015-09-23 20:12:29 -0400

    Closing the first minor release (0.1-dev)

--- src/FourStreamServiceProvider.php
+++ src/FourStreamServiceProvider.php
@@ -2,7 +2,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2014 Anton Samuelsson
+ * Copyright (c) 2007-2015 YnievesDotNet <yoinier.hn@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
