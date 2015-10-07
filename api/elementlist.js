e0694e1149362298096c98df42b156f1fe551389
Yoinier Hernandez Nieves <yoinier.hn@gmail.com>
2015-10-07 13:31:47 -0700

    Releasing the version 0.4

--- api/elementlist.js
+++ api/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","YnievesDotNet\\FourStream\\Command\\FourStreamStartCommand"],["c","YnievesDotNet\\FourStream\\Facades\\FourStream"],["c","YnievesDotNet\\FourStream\\FourStream"],["c","YnievesDotNet\\FourStream\\FourStream\\FourStreamServer"],["c","YnievesDotNet\\FourStream\\FourStreamServiceProvider"],["c","YnievesDotNet\\FourStream\\Models\\FourStreamTocken"],["c","YnievesDotNet\\FourStream\\Traits\\FourStreamUserTockens"]];
+ApiGen.elements = [["c","YnievesDotNet\\FourStream\\Command\\FourStreamStartCommand"],["c","YnievesDotNet\\FourStream\\Facades\\FourStream"],["c","YnievesDotNet\\FourStream\\FourStream"],["c","YnievesDotNet\\FourStream\\FourStream\\FourStreamServer"],["c","YnievesDotNet\\FourStream\\FourStreamServiceProvider"],["c","YnievesDotNet\\FourStream\\Models\\FourStreamNode"],["c","YnievesDotNet\\FourStream\\Models\\FourStreamTag"],["c","YnievesDotNet\\FourStream\\Traits\\FourStreamUserNodes"]];
